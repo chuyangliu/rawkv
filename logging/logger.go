4c049bb2d7245a1439cd6ed590565afd295dac13
Chuyang Liu <chuyang.s.liu@gmail.com>
2020-01-07 10:21:03 +0800

    [logging] add log levels

--- logging/logger.go
+++ logging/logger.go
@@ -6,36 +6,54 @@ import (
 	"os"
 )
 
+// Candidate values for logging level.
+const (
+	LevelDebug = iota
+	LevelInfo  = iota
+	LevelWarn  = iota
+	LevelError = iota
+)
+
 // Logger formats and writes log messages.
 type Logger struct {
 	logger *log.Logger
+	level  int
 }
 
 // New instantiates a Logger.
-func New() *Logger {
+func New(level int) *Logger {
 	return &Logger{
 		logger: log.New(os.Stderr, "", log.LstdFlags|log.LUTC|log.Lshortfile),
+		level:  level,
 	}
 }
 
 // Debug writes formatted log with debug messages.
 func (l *Logger) Debug(format string, operands ...interface{}) {
-	l.write("DEBUG", format, operands...)
+	if l.level >= LevelDebug {
+		l.write("DEBUG", format, operands...)
+	}
 }
 
 // Info writes formatted log with information messages.
 func (l *Logger) Info(format string, operands ...interface{}) {
-	l.write("INFO", format, operands...)
+	if l.level >= LevelInfo {
+		l.write("INFO", format, operands...)
+	}
 }
 
 // Warn writes formatted log with warning messages.
 func (l *Logger) Warn(format string, operands ...interface{}) {
-	l.write("WARN", format, operands...)
+	if l.level >= LevelWarn {
+		l.write("WARN", format, operands...)
+	}
 }
 
 // Error writes formatted log with error messages.
 func (l *Logger) Error(format string, operands ...interface{}) {
-	l.write("ERROR", format, operands...)
+	if l.level >= LevelError {
+		l.write("ERROR", format, operands...)
+	}
 }
 
 func (l *Logger) write(typ string, format string, operands ...interface{}) {
