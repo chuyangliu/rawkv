ef9bac3a5158e36cf332e9b2fa37fd6dde6decdf
Chuyang Liu <chuyang.s.liu@gmail.com>
2020-01-17 09:38:02 -0800

    [rawkvsvr] update docker image and kube config

--- pkg/cmd/rawkvsvr/main.go
+++ pkg/cmd/rawkvsvr/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	storageAddr := flag.String("storageaddr", "127.0.0.1:8000", "Address for storage server to listen.")
 	raftAddr := flag.String("raftaddr", "127.0.0.1:8001", "Address for raft server to listen.")
-	rootdir := flag.String("rootdir", "./server-root", "Root directory to persist data.")
+	rootdir := flag.String("rootdir", "./rawkv-root", "Root directory to persist data.")
 	flushThresh := flag.Uint64("flushthresh", uint64(1)<<25, "Threshold in bytes to flush MemStore.")
 	blkSize := flag.Uint64("blocksize", uint64(1)<<18, "Block size in bytes to persist FileStore.")
 	level := flag.Int("loglevel", 1, "Log level (0/1/2/3 => Debug/Info/Warn/Error).")
